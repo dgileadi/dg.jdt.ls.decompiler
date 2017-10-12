8913ff0f2ce44fb8029b540e10e93b944d3683e7
David Gileadi <gileadis@gmail.com>
2017-10-12 16:54:04 -0700

    Update FernflowerDecompiler to use latest

--- dg.jdt.ls.decompiler.fernflower/src/dg/jdt/ls/decompiler/fernflower/BytecodeProvider.java
+++ dg.jdt.ls.decompiler.fernflower/src/dg/jdt/ls/decompiler/fernflower/BytecodeProvider.java
@@ -11,26 +11,59 @@
 package dg.jdt.ls.decompiler.fernflower;
 
 import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.eclipse.jdt.core.IClassFile;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.ls.core.internal.JDTUtils;
 import org.jetbrains.java.decompiler.main.extern.IBytecodeProvider;
 
 class BytecodeProvider implements IBytecodeProvider {
 
-	private final IClassFile classFile;
+	private URI uri;
+	private byte[] bytecode;
+
+	public BytecodeProvider(URI uri) {
+		this.uri = uri;
+	}
 
 	public BytecodeProvider(IClassFile classFile) {
-		this.classFile = classFile;
+		try {
+			bytecode = loadClassFileBytes(classFile);
+		} catch (IOException e) {}
 	}
 
 	@Override
 	public byte[] getBytecode(String externalPath, String internalPath) throws IOException {
+		if (bytecode != null) {
+			return bytecode;
+		}
+		bytecode = loadClassFileBytes(JDTUtils.resolveClassFile(uri));
+		if (bytecode == null) {
+			bytecode = loadBytecode(uri);
+		}
+		return bytecode;
+	}
+
+	private byte[] loadBytecode(URI uri) throws IOException {
+		if (uri == null) {
+			return null;
+		}
+		Path path = Paths.get(uri);
+		return Files.readAllBytes(path);
+	}
+
+	private byte[] loadClassFileBytes(IClassFile classFile) throws IOException {
+		if (classFile == null) {
+			return null;
+		}
 		try {
 			return classFile.getBytes();
 		} catch (JavaModelException e) {
-			throw new IOException();
+			throw new IOException(e);
 		}
 	}
-	
 }
